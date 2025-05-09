6fbb66edd4eed33022a9cae6f712431e8747cc12
Dorapocket <993549790@qq.com>
2025-05-09 21:59:58 +0800

    fix script

--- scripts/group_img_fix.js
+++ scripts/group_img_fix.js
@@ -1,4 +1,5 @@
 var code = `
+<script>
 let gis = document.getElementsByClassName("group-image-wrap");
 for (let i = 0; i < gis.length; i++) {
     let gi = gis[i];
@@ -8,6 +9,7 @@ for (let i = 0; i < gis.length; i++) {
         img.parentElement.href = img.parentElement.href.replace("https://www.lgyserver.top/", "");
         img.src = img.src.replace("https://www.lgyserver.top/", "");
     }
-}`;
+}
+</script>`;
 
 hexo.extend.injector.register('body_end', code, 'post');
